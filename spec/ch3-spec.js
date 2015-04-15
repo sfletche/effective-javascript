8a173acef50a0771309b7234a70a3d684bc74f15
sfletche <sfletche@gmail.com>
2015-04-14 21:39:34 -0700

    stubs for ch3

--- spec/ch3-spec.js
+++ spec/ch3-spec.js
@@ -2,3 +2,47 @@
 describe( "Item 18: Understanding the Difference between Function, Method, and Constructor Calls...", function() {
 
 });
+
+describe( "Item 19: Get comfortable Using Higher-Order Functions...", function() {
+
+});
+
+describe( "Item 20: Use call to Call methods with a Custom Receiver...", function() {
+
+});
+
+describe( "Item 21: Use apply to Call Functions with Different Numbers of Arguments...", function() {
+
+});
+
+describe( "Item 22: Use arguments to Create Variadic Functions...", function() {
+
+});
+
+describe( "Item 23: Never Modify the arguments Object...", function() {
+
+});
+
+describe( "Item 24: Use a Variable to Save a Reference to arguments...", function() {
+
+});
+
+describe( "Item 25: Use bind to Extract Methods with a Fixed Receiver...", function() {
+
+});
+
+describe( "Item 26: Use bind to Curry Functions...", function() {
+
+});
+
+describe( "Item 27: Prefer Closures to Strings for Encapsulating Code...", function() {
+
+});
+
+describe( "Item 28: Avoid Relying on the toString Method of Functions...", function() {
+
+});
+
+describe( "Item 29: Avoid Nonstandard Stack Inspection Properties...", function() {
+
+});
