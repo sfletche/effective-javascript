71a8533696e4b6a6bf772eee7dd525c3bec18eea
sfletche <sfletche@gmail.com>
2015-04-15 20:53:29 -0700

    adding a couple tests for ch3

--- spec/ch3-spec.js
+++ spec/ch3-spec.js
@@ -1,14 +1,66 @@
 
 describe( "Item 18: Understanding the Difference between Function, Method, and Constructor Calls...", function() {
 
+	var stateYourName = function() {
+		return this.username;
+	};
+	var user = {
+		username: 'Bruce',
+		stateYourName: stateYourName
+	};
+	var User = function(username) {
+		this.username = username;
+		this.stateYourName = stateYourName;
+	};
+
+	var callingFunctions = function() {
+		return stateYourName();
+	}
+	var callingMethods = function() {
+		return stateYourName();
+	};
+	var callingConstructors = function() {
+		var user = User('Bruce');
+		return user.stateYourName;
+	};
+
+	it("callingFunctions() demonstrates why using 'this' in functions (as opposed to methods and constructors) should be avoided...", function() {
+		expect(callingFunctions()).toBe(undefined);
+	});
+	it("callingMethods() should be fixed so it uses an appropriate receiver...", function() {
+		expect(callingMethods()).toEqual("Bruce");
+	});
+	it("callingConstructors() should be fixed so it calls the constructor correctly...", function() {
+		expect(callingConstructors()).toEqual("Bruce");
+	});
+
 });
 
 describe( "Item 19: Get comfortable Using Higher-Order Functions...", function() {
 
+	var numbers = [5, 2, 3, 1, 4];
+	var reverse = function() {
+		return numbers.sort(function(x, y) {
+			return true;
+		});
+	};
+	var square = function() {
+		return numbers.map(function(x) {
+			return x;
+		});
+	};
+
+	it("reverse() should return the array [5, 4, 3, 2, 1]...", function() {
+		expect(reverse()).toEqual([5,4,3,2,1]);
+	});
+	it("square() should return the array [25, 4, 9, 1, 16]...", function() {
+		expect(square()).toEqual([25,4,9,1,16]);
+	});
+
 });
 
 describe( "Item 20: Use call to Call methods with a Custom Receiver...", function() {
-
+	
 });
 
 describe( "Item 21: Use apply to Call Functions with Different Numbers of Arguments...", function() {
