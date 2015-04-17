61fe73fba91489c71dd4cbb263e468abeb30dde7
sfletche <sfletche@gmail.com>
2015-04-16 20:42:59 -0700

    stubs for chapter 4

---
+++ spec/ch4-spec.js
@@ -0,0 +1,52 @@
+
+describe( "Item 30: Understand the Difference between prototype, getPrototypeOf, and __proto__...", function() {
+	
+});
+
+describe( "Item 31: Prefer Object.getPrototypeOf to __proto__...", function() {
+
+});
+
+describe( "Item 32: Never modify __proto__...", function() {
+
+});
+
+describe( "Item 33: Make Your Constructors new-Agnostic...", function() {
+
+});
+
+describe( "Item 34: Store Methods on Prototype...", function() {
+
+});
+
+describe( "Item 35: Use Closures to Store Private Data...", function() {
+
+});
+
+describe( "Item 36: Store Instance State Only on Instance Objects...", function() {
+
+});
+
+describe( "Item 37: Recognize the Implicit Binding of this...", function() {
+
+});
+
+describe( "Item 38: Call Superclass Constructors from Subclass Constructors...", function() {
+
+});
+
+describe( "Item 39: Never Reuse Superclass Property Names...", function() {
+
+});
+
+describe( "Item 40: Avoid Inheriting from Standard Classes...", function() {
+
+});
+
+describe( "Item 41: Treat Prototype As an Implementation Detail...", function() {
+
+});
+
+describe( "Item 42: Avoid Reckless Monkey-Patching...", function() {
+
+});
