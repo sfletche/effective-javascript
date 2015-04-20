a55262a96c17de2803144e9bb7e9223f52807f7a
sfletche <sfletche@gmail.com>
2015-04-19 20:25:04 -0700

    more items from chapter 5

--- spec/ch5-spec.js
+++ spec/ch5-spec.js
@@ -2,3 +2,11 @@
 describe( "Item 43: Build Lightweight Dictionaries from Direct Instances of Object...", function() {
 	
 });
+
+describe( "Item 44: Use null Prototypes to Prevent Prototype Pollution...", function() {
+
+});
+
+describe( "Item 45: Use hasOwnProperty to Protect Against Prototype Pollution...", function() {
+
+});
