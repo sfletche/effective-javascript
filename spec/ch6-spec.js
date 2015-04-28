bd6377f31e121af30c40a281669a70a9b5f03c97
sfletche <sfletche@gmail.com>
2015-04-27 20:24:26 -0700

    stubbing Items 54 and 55

--- spec/ch6-spec.js
+++ spec/ch6-spec.js
@@ -2,3 +2,11 @@
 describe( "Item 53: Maintain Consistent Conventions...", function() {
 	
 });
+
+describe( "Item 54: Treat undefined As 'No Value'...", function() {
+
+});
+
+describe( "Item 55: Accept Options Objects for Keyword Arguments...", function() {
+
+});
