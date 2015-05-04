4032f2a53eeeb5719d85bf789659d5e33d26ddbb
sfletche <sfletche@gmail.com>
2015-05-03 20:21:20 -0700

    stubbing Items 62

--- spec/ch7-spec.js
+++ spec/ch7-spec.js
@@ -2,3 +2,7 @@
 describe( "Item 61: Don't Block the Event Queue on I/O...", function() {
 	
 });
+
+describe( "Item 62: Use Nested or Named Callbacks for Asynchronous Sequencing...", function() {
+	
+});
